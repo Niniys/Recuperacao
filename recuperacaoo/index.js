5fe75114b2bb39e703c5568c5f8ed677c5ccf055
Niniys <82469818+Niniys@users.noreply.github.com>
2021-12-20 11:27:58 -0300

    Add files via upload

--- recuperacaoo/index.js
+++ recuperacaoo/index.js
@@ -1,43 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native'
-import style from './style.js'
-import { StatusBar } from 'expo-status-bar';
-
-export default function Home({ navigation }) {
-
-    const [lista, setlista] = useState([
-        {
-            "name": "pokemon",
-            "url": "https://pokeapi.co/api/v2/pokemon/0/"
-        }
-    ])
-
-    useEffect(() =>{
-        fetch("https://pokeapi.co/api/v2/pokemon/", {
-            "method": "GET",
-            "headers": {
-                Accept: 'aplication/json',
-                'Content-Type': 'aplication/json'
-            }
-        }).then(resp => {
-            return resp.json()
-        }).then(data => {
-            setlista(data);
-        });
-    }, [lista]);
-
-    return(
-        <View style={style.pag}>
-
-        <StatusBar style="menu" />
-
-        {lista.map((item, index) =>
-
-            <TouchableOpacity style={style.menu} key={index} onPress={() => { navigation.navigate('Detalhes', item) }}>
-                <Text>name: {item.name}</Text>
-                <Text>url: {item.url}</Text>
+import React from 'react';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import style from './style.js'; export default function Detalhes({ navigation, route }) {
+    const { name, url, is_hidden, slot } = route.params;
+    return (
+        <View>
+            <TouchableOpacity style={style.card}>
+                <View>
+                    <Text style={{ fontWeight: 'bold' }}>Nome:</Text>
+                    <Text>{name}</Text>
+                </View>
+                <View>
+                    <Text style={{ fontWeight: 'bold' }}>url:</Text>
+                    <Text>{url}</Text>
+                </View>
+                <View>
+                    <Text style={{ fontWeight: 'bold' }}>habilidade:</Text>
+                    <Text>{is_hidden}</Text>
+                </View>
+                <View>
+                    <Text style={{ fontWeight: 'bold' }}>:</Text>
+                    <Text>{slot}</Text>
+                </View>
+                <View>
+                    <Text style = {style.img} style={{ fontWeight: 'bold' }}>Pokemon:</Text>
+                    <Text>{front_default}</Text>
+                </View>
             </TouchableOpacity>
-        )}
         </View>
     );
 }
